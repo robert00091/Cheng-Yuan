8ed07b2a504ddd604f606b9a1a425af740f72831
robert00091 <44386762+robert00091@users.noreply.github.com>
2020-09-27 03:31:55 +0800

    Update HW2.py

--- Algorithm/Computation in Data Science/HW2.py
+++ Algorithm/Computation in Data Science/HW2.py
@@ -1,5 +1,6 @@
 import numpy
 import random
+import math
 
 global x_1, x_2
 
@@ -23,6 +24,8 @@ def random_sampling(x_1, x_2):
 
     print(xb)
     print('min:', f(xb))
+
+    return xb
 
 
 
@@ -52,13 +55,59 @@ def rand_walk(x_1, x_2):
 
     print(x_best)
     print('min:', f(x_best))
-             
+    
+    return x_best
+
+# Simulated Annealing
+def simul_anneling(X):
+    pcur = f(X)
+    #print(X)
+    pbest = pcur
+    T = 100000
+    c = 0.2
+    pb = [0]*2
+    move_1, move_2 = [], []
 
 
+    count = 0
+    while count < 50 and T > 0:
+        for dist in numpy.arange(-1,1,0.5):
+            x_1 = X[0] + dist
+            x_2 = X[1] + dist
+            move_1.append(x_1)
+            move_2.append(x_2)
+            #print(move)
 
+        for i in move_1:
+            for j in move_2:
+                pb[0], pb[1] = i, j
+                #print(pb)
+                pnew = f(pb)
+
+                dE = pnew - pcur
+                if dE <= 0:
+                    X[0], X[1] = pb[0], pb[1]
+                    pcur = pnew
+                    if pcur < pbest:
+                        pbest = pcur
+                # dE > 0: using the Metropolis criterion
+                else:        
+                    rv = random.uniform(0,1)
+                    if rv < math.exp(-dE/T):
+                        pcur = pnew
+                        T = T * c
+                    
+        count = count + 1
+
+    print(X)
+    print(pbest)
 
 if __name__ == "__main__":
     x_1, x_2 = 20, 0
-    random_sampling(x_1, x_2)
+    X_1 = random_sampling(x_1, x_2)
 
-    rand_walk(x_1, x_2)
+    print('Random walk:')
+    X = rand_walk(x_1, x_2)
+
+    print('Simulated annealing:')
+    simul_anneling(X_1)
